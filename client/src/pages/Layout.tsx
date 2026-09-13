24e48338224369cfc4b96219674f839b8ebde610
Ashtika <aashtika2002@gmail.com>
2026-09-13 18:46:58 +0530

    craeted sidebar and mobile menu

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,9 +1,15 @@
 import { Outlet } from "react-router-dom"
+import Sidebar from "../components/Sidebar"
+import BottomNav from "../components/BottomNav"
 
 const Layout = () => {
   return (
-    <div>
-      <Outlet/>
+    <div className="layout-container">
+      <Sidebar/>
+      <div className="flex-1 overflow-y-scroll">
+        <Outlet/>
+      </div>
+      <BottomNav/> 
     </div>
   )
 }
